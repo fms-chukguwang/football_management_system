917725b02a4d2847ef3f1fd46abc1d4c5810635e
HoyeongJeon <hoyeong0302@gmail.com>
2024-01-23 13:50:30 +0900

    feat: deletUserById softdelete 수정

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Query,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Query, Req, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { PaginateUserDto } from './dto/paginate-user.dto';
@@ -27,49 +18,48 @@ import { UserService } from 'src/user/user.service';
 @UseGuards(JwtAuthGuard, IsAdminGuard)
 @Controller('admin')
 export class AdminController {
-  constructor(
-    private readonly adminService: AdminService,
-    private readonly userService: UserService,
-  ) {}
+    constructor(
+        private readonly adminService: AdminService,
+        private readonly userService: UserService,
+    ) {}
 
-  /**
-   * 팀 목록 가져오기(전체 팀, 페이징 적용)
-   * @param dto
-   * @returns
-   */
-  @ApiBearerAuth()
-  @Get('teams')
-  async getAllTeams(@Query() dto: PaginateTeamDto) {
-    return await this.adminService.getAllTeams(dto);
-  }
-
-  /**
-   * 유저 목록 가져오기(전체 유저, 페이징 적용)
-   * @param dto
-   * @returns
-   */
-  @ApiBearerAuth()
-  @Get('users')
-  async getAllUsers(@Query() dto: PaginateUserDto, @Req() req: Request) {
-    return await this.adminService.paginateUser(dto);
-  }
-
-  // 추가적인 로직 필요할듯? User에 조회, 삭제 없애야할듯
-  /**
-   * 유저 삭제하기
-   * @param dto
-   * @returns
-   */
-  @ApiBearerAuth()
-  @Delete('/:dataType/:userId')
-  async deleteUserById(
-    @Param('dataType') dataType: string,
-    @Param('userId') userId: number,
-  ) {
-    if (dataType === 'users') {
-      return await this.userService.deleteId(userId);
-    } else if (dataType === 'teams') {
-      // return await this.adminService.deleteTeamById(userId);
+    /**
+     * 팀 목록 가져오기(전체 팀, 페이징 적용)
+     * @param dto
+     * @returns
+     */
+    @ApiBearerAuth()
+    @Get('teams')
+    async getAllTeams(@Query() dto: PaginateTeamDto) {
+        return await this.adminService.getAllTeams(dto);
     }
-  }
+
+    /**
+     * 유저 목록 가져오기(전체 유저, 페이징 적용)
+     * @param dto
+     * @returns
+     */
+    @ApiBearerAuth()
+    @Get('users')
+    async getAllUsers(@Query() dto: PaginateUserDto, @Req() req: Request) {
+        return await this.adminService.paginateUser(dto);
+    }
+
+    // 추가적인 로직 필요할듯? User에 조회, 삭제 없애야할듯
+    /**
+     * 유저 삭제하기
+     * @param dto
+     * @returns
+     */
+    @ApiBearerAuth()
+    @Delete('/:dataType/:userId')
+    async deleteUserById(@Param('dataType') dataType: string, @Param('userId') userId: number) {
+        if (dataType === 'users') {
+            console.log('userId');
+            console.log(userId);
+            return await this.userService.deleteId(userId);
+        } else if (dataType === 'teams') {
+            // return await this.adminService.deleteTeamById(userId);
+        }
+    }
 }
