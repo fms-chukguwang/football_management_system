5b01998e0ef6194a815cb35b8128c6b64957f895
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-01-24 20:11:18 +0900

    pagenation

--- src/admin/dto/paginate-user.dto.ts
+++ src/admin/dto/paginate-user.dto.ts
@@ -1,3 +1,5 @@
 import { BasePaginationDto } from '../../common/dto/base-pagination.dto';
 
-export class PaginateUserDto extends BasePaginationDto {}
+export class PaginateUserDto extends BasePaginationDto {
+    take=5
+}
