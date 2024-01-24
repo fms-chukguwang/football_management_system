0465e2f74908601bcc6c96fbf80d20c8b67e25bd
HoyeongJeon <hoyeong0302@gmail.com>
2024-01-24 16:43:02 +0900

    fix: swr 요청 로깅 제외, 로깅 메세지 수정

--- src/common/exception-filter/http.exception-filter.ts
+++ src/common/exception-filter/http.exception-filter.ts
@@ -1,41 +1,38 @@
-import {
-  ArgumentsHost,
-  Catch,
-  ExceptionFilter,
-  HttpException,
-} from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { LoggingService } from 'src/logging/logging.service';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  constructor(private readonly myLogger: LoggingService) {}
-  catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const res = ctx.getResponse();
-    const req = ctx.getRequest();
-    const status = exception.getStatus();
-    const err = exception.getResponse() as
-      | { message: any; statusCode: number; error: number }
-      | { error: number; statusCode: 400; message: string[] }; // class-validator 타이핑
+    constructor(private readonly myLogger: LoggingService) {}
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const res = ctx.getResponse();
+        const req = ctx.getRequest();
+        const status = exception.getStatus();
+        const err = exception.getResponse() as
+            | { message: any; statusCode: number; error: number }
+            | { error: number; statusCode: 400; message: string[] }; // class-validator 타이핑
+        console.log(err.message);
+        if (req.url !== '/api/users/me') {
+            this.myLogger.error(
+                ' method : ' +
+                    req.method +
+                    'status : ' +
+                    status +
+                    ' error : ' +
+                    err.message +
+                    ' path : ' +
+                    req.url,
+            );
+        }
 
-    this.myLogger.error(
-      ' method : ' +
-        req.method +
-        'status : ' +
-        status +
-        ' error : ' +
-        err.message +
-        ' path : ' +
-        req.url,
-    );
-
-    res.status(status).json({
-      statusCode: status,
-      message: err.message,
-      timestamp: new Date().toLocaleString('ko-KR', {
-        timeZone: 'Asia/Seoul',
-      }),
-      path: req.url,
-    });
-  }
+        res.status(status).json({
+            statusCode: status,
+            message: err.message,
+            timestamp: new Date().toLocaleString('ko-KR', {
+                timeZone: 'Asia/Seoul',
+            }),
+            path: req.url,
+        });
+    }
 }
--- src/common/interceptors/log.interceptor.ts
+++ src/common/interceptors/log.interceptor.ts
@@ -1,66 +1,65 @@
 // 로그 인터셉터
-import {
-  CallHandler,
-  ExecutionContext,
-  Injectable,
-  Logger,
-  NestInterceptor,
-} from '@nestjs/common';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
 import { Observable, map, observable, tap } from 'rxjs';
 import { LoggerMiddleware } from '../middleware/logger.middleware';
 import { LoggingService } from 'src/logging/logging.service';
 
 @Injectable()
 export class LogInterceptor implements NestInterceptor {
-  constructor(private readonly myLogger: LoggingService) {}
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> {
-    const now = new Date(); // 현재 시간과 날짜
-    const req = context.switchToHttp().getRequest();
-    const path = req.originalUrl;
-    const method = req.method;
+    constructor(private readonly myLogger: LoggingService) {}
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+        const now = new Date(); // 현재 시간과 날짜
+        const req = context.switchToHttp().getRequest();
+        const path = req.originalUrl;
+        const method = req.method;
 
-    console.log(
-      `[REQ] ${method} ${path} ${now.toLocaleString('ko-KR', {
-        timeZone: 'Asia/Seoul',
-      })}`,
-    );
-    if (
-      method === 'GET' ||
-      method === 'POST' ||
-      method === 'PUT' ||
-      method === 'DELETE' ||
-      method === 'PATCH'
-    ) {
-      this.myLogger.log(
-        `[REQ] ${method} ${path} ${now.toLocaleString('ko-KR', {
-          timeZone: 'Asia/Seoul',
-        })}`,
-      );
-    }
-    return next.handle().pipe(
-      tap(() => {
         console.log(
-          `[RES] ${method} ${path} ${new Date().toLocaleString('ko-KR', {
-            timeZone: 'Asia/Seoul',
-          })} ${new Date().getMilliseconds() - now.getMilliseconds()}ms`,
+            `[REQ] ${method} ${path} ${now.toLocaleString('ko-KR', {
+                timeZone: 'Asia/Seoul',
+            })}`,
         );
-        if (
-          method === 'GET' ||
-          method === 'POST' ||
-          method === 'PUT' ||
-          method === 'DELETE' ||
-          method === 'PATCH'
-        ) {
-          this.myLogger.log(
-            `[RES] ${method} ${path} ${new Date().toLocaleString('ko-KR', {
-              timeZone: 'Asia/Seoul',
-            })} ${new Date().getMilliseconds() - now.getMilliseconds()}ms`,
-          );
-        }
-      }),
-    );
-  }
+        // if (
+        //     method === 'GET' ||
+        //     method === 'POST' ||
+        //     method === 'PUT' ||
+        //     method === 'DELETE' ||
+        //     method === 'PATCH'
+        // ) {
+        //     this.myLogger.log(
+        //         `[REQ] ${method} ${path} ${now.toLocaleString('ko-KR', {
+        //             timeZone: 'Asia/Seoul',
+        //         })}`,
+        //     );
+        // }
+        return next.handle().pipe(
+            tap((observable) => {
+                console.log(
+                    `[RES] ${method} ${path} ${new Date().toLocaleString('ko-KR', {
+                        timeZone: 'Asia/Seoul',
+                    })} ${new Date().getMilliseconds() - now.getMilliseconds()}ms
+                    [RETURN] ${JSON.stringify(observable)}`,
+                );
+                if (
+                    method === 'GET' ||
+                    method === 'POST' ||
+                    method === 'PUT' ||
+                    method === 'DELETE' ||
+                    method === 'PATCH'
+                ) {
+                    if (path !== '/api/users/me') {
+                        this.myLogger.log(
+                            `
+                            [REQ] ${method} ${path} ${now.toLocaleString('ko-KR', {
+                                timeZone: 'Asia/Seoul',
+                            })}
+                            [RES] ${method} ${path} ${new Date().toLocaleString('ko-KR', {
+                                timeZone: 'Asia/Seoul',
+                            })} ${new Date().getMilliseconds() - now.getMilliseconds()}ms
+                            [RETURN] ${JSON.stringify(observable)}`,
+                        );
+                    }
+                }
+            }),
+        );
+    }
 }
