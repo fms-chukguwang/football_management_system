15bb0fd96a77fde28747faa8d02ce25ffe21e2f5
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-01-25 12:00:26 +0900

    Revert "refactoring"
    This reverts commit 84db88b902f5bbe4c9d960cf5c2f4f2b523b50a5.

--- src/member/entities/member.entity.ts
+++ src/member/entities/member.entity.ts
@@ -41,9 +41,8 @@ export class Member extends BaseModel {
     isStaff: boolean;
 
     @CreateDateColumn({
-        type: 'timestamp',
-        precision: 6,
-        default: () => 'CURRENT_TIMESTAMP(6)',
+        type: 'date',
+        name: 'join_date',
     })
     joinDate: Date;
 }
