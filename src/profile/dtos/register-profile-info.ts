442e8a1ac2fd73b740685df75b5c647167c68e5b
HoyeongJeon <hoyeong0302@gmail.com>
2024-01-31 22:14:53 +0900

    feat:프로필 사진 등록 백엔드 수정

--- src/profile/dtos/register-profile-info.ts
+++ src/profile/dtos/register-profile-info.ts
@@ -1,46 +1,46 @@
-import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { Position } from '../../enums/position.enum';
 import { Gender } from '../../enums/gender.enum';
-
+import { Column } from 'typeorm';
 
 export class RegisterProfileInfoDto {
- /**
-   * 포지션
-   * @example "Center Back"
-   */
-  @IsEnum(Position, { message: 'Please provide a valid position value' })
-  @IsNotEmpty({ message: 'Preferred position is required' })
-  preferredPosition: Position;
+    /**
+     * 포지션
+     * @example "Center Back"
+     */
+    @IsEnum(Position, { message: 'Please provide a valid position value' })
+    @IsNotEmpty({ message: 'Preferred position is required' })
+    preferredPosition: Position;
 
-  /**
-   * 몸무게
-   * @example 59
-   */
-  @IsNumber()
-  @IsNotEmpty({ message: 'Please provide weight' })
-  weight: number;
+    /**
+     * 몸무게
+     * @example 59
+     */
+    @IsNumber()
+    @IsNotEmpty({ message: 'Please provide weight' })
+    weight: number;
 
-  /**
-   * 키
-   * @example 159
-   */
-  @IsNumber()
-  @IsNotEmpty({ message: 'Please provide height' })
-  height: number;
+    /**
+     * 키
+     * @example 159
+     */
+    @IsNumber()
+    @IsNotEmpty({ message: 'Please provide height' })
+    height: number;
 
-  /**
-   * 나이
-   * @example 18
-   */
-  @IsNumber()
-  @IsNotEmpty({ message: 'Please provide age' })
-  age: number;
+    /**
+     * 나이
+     * @example 18
+     */
+    @IsNumber()
+    @IsNotEmpty({ message: 'Please provide age' })
+    age: number;
 
-  /**
-   * 성별
-   * @example "Male"
-   */
-  @IsEnum(Gender, { message: 'Please provide a valid gender value' })
-  @IsNotEmpty({ message: 'Please provide gender' })
-  gender: Gender;
+    /**
+     * 성별
+     * @example "Male"
+     */
+    @IsEnum(Gender, { message: 'Please provide a valid gender value' })
+    @IsNotEmpty({ message: 'Please provide gender' })
+    gender: Gender;
 }
