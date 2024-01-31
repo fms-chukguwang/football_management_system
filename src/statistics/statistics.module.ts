8531b7b9e2dd7e6b1f51809e3fe1c95700880edf
kim <iufan1526@gmail.com>
2024-01-31 09:57:11 +0900

    feat : 팀스탯 관련 로직 일부분 추가

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -4,9 +4,10 @@ import { StatisticsController } from './statistics.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeamStats } from 'src/match/entities/team-stats.entity';
 import { MatchResult } from 'src/match/entities/match-result.entity';
+import { PlayerStats } from 'src/match/entities/player-stats.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([TeamStats, MatchResult])],
+    imports: [TypeOrmModule.forFeature([TeamStats, MatchResult, PlayerStats])],
     controllers: [StatisticsController],
     providers: [StatisticsService],
 })
