4f9eac0f1a7336bf1852158d2c2133f27f244fc7
=lemon-table <=>
2024-02-06 00:52:12 +0900

    feat:경기장 조회 페이지네이션 적용

--- src/soccerfield/soccerfield.service.ts
+++ src/soccerfield/soccerfield.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SoccerFieldModel } from './entities/soccerfield.entity';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, FindManyOptions, Like, Repository } from 'typeorm';
+import { PaginateFieldDto } from './dtos/paginate-field-dto';
+import { CommonService } from '../common/common.service';
+import { SoccerField } from '../match/entities/soccer-field.entity';
 
 @Injectable()
 export class SoccerfieldService {
-    constructor(private readonly dataSource: DataSource) {}
+    constructor(
+
+        @InjectRepository(SoccerField)
+        private soccerFieldRepository: Repository<SoccerField>,
+        
+        private readonly dataSource: DataSource,
+        private readonly commonService: CommonService
+        
+        
+        ) {}
     /**
      * 경기장 가져오기
      */
@@ -17,5 +29,56 @@ export class SoccerfieldService {
             .getOne();
         console.log('stadium', stadium);
         return stadium;
+    }
+
+    /**
+     * 경기장 전체 조회
+     * @returns
+     */
+    async findAllStadium(userId: number, dto: PaginateFieldDto, name?: string) {
+
+        // const queryOptions = {
+        //     relations: {
+        //         locationfield: true,
+        //     },
+        //     select: {
+        //         locationfield: {
+        //             address: true,
+        //             state: true,
+        //             city: true,
+        //             district: true,
+        //         },
+        //     },
+        // };
+    
+        // // name 값이 제공되었다면 where 조건 추가
+        // if (name) {
+        //     queryOptions['where'] = {
+        //         field_name: Like(`%${name}%`)
+        //     };
+        // }
+
+        // const soccerField = await this.soccerFieldRepository.find(queryOptions);
+
+        console.log('dto:',dto);
+        console.log('name:',name);
+
+        const options: FindManyOptions<SoccerField> = {
+            relations: {
+                locationfield: true,
+            }
+        };
+
+        if (name) {
+            options.where = {  field_name: Like(`%${name}%`)  };
+        }
+        
+
+        if (!options) {
+            throw new NotFoundException('등록된 경기장 목록이 없습니다.');
+        }
+
+        //return soccerField;
+        return await this.commonService.paginate(dto, this.soccerFieldRepository, options, 'soccerfield');
     }
 }
