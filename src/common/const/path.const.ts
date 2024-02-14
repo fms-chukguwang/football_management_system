90efb686b80644b7aa0044cbc03fefbb47638839
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2024-02-14 19:41:28 +0900

    feat: email debugging

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -1,7 +1,7 @@
 export const PROTOCOL = 'http';
 export const HOST = 'localhost:3000';
 export const NODE_ENV = process.env.NODE_ENV;
-let SERVER_URL;
+export let SERVER_URL;
 if (NODE_ENV == 'local') {
     SERVER_URL = `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`;
 } 
