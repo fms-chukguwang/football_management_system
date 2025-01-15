095ed61c5f4604f6e3e3fb78e892a18105860e15
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-15 22:24:16 +0900

    refactor: WsExceptionFilter 클래스 단위로 적용

--- src/chats/guard/ws-bearer-token.guard.ts
+++ src/chats/guard/ws-bearer-token.guard.ts
@@ -6,24 +6,21 @@ import { ConfigService } from '@nestjs/config';
 // token에 있는 유저 ID 갖고오기( 소켓에서! )
 @Injectable()
 export class SocketBearerTokenGuard implements CanActivate {
-  constructor(private readonly configService: ConfigService) {}
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const socket = context.switchToWs().getClient();
-    const headers = socket.handshake.headers;
-    const bearerToken = headers['authorization'];
-    const rawToken = bearerToken.split(' ')[1];
-    if (!rawToken) {
-      throw new WsException('Token not found');
+    constructor(private readonly configService: ConfigService) {}
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const socket = context.switchToWs().getClient();
+        const headers = socket.handshake.headers;
+        const bearerToken = headers['authorization'];
+        const rawToken = bearerToken.split(' ')[1];
+        if (!rawToken) {
+            throw new WsException('Token not found');
+        }
+        try {
+            const decoded = jwt.verify(rawToken, this.configService.get<string>('JWT_SECRET'));
+            socket.userId = decoded['id'];
+            return true;
+        } catch (e) {
+            throw new WsException('Token is invalid');
+        }
     }
-    try {
-      const decoded = jwt.verify(
-        rawToken,
-        this.configService.get<string>('JWT_SECRET'),
-      );
-      socket.userId = decoded['id'];
-      return true;
-    } catch (e) {
-      throw new WsException('Token is invalid');
-    }
-  }
 }
